2bda6909b1dafe94261ab39a30e3d1b0bdbaebf3
karama gueye <karamagueye04@gmail.com>
2023-10-16 04:19:03 -0700

    printf

--- printf/_printf.c
+++ printf/_printf.c
@@ -1,46 +1,67 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "main.h"
 
-int _printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-    int charCount = 0;
+void print_buffer(char buffer[], int *buff_ind);
 
-    while (*format) {
-        if (*format != '%') {
-            putchar(*format);
-            charCount++;
-        } else {
-            format++;  // Move past the '%'
-            if (*format == 'c') {
-                char c = va_arg(args, int);
-                putchar(c);
-                charCount++;
-            } else if (*format == 's') {
-                char *str = va_arg(args, char*);
-                if (str) {
-                    while (*str) {
-                        putchar(*str);
-                        str++;
-                        charCount++;
-                    }
-                }
-            } else if (*format == '%') {
-                putchar('%');
-                charCount++;
-            }
-        }
-        format++;
-    }
+/**
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
+int _printf(const char *format, ...)
+{
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-    va_end(args);
-    return charCount;
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
 }
 
-int main() {
-    _printf("Hello, %s! This is a simple %c%c%c example.\n", "World", 'p', 'r', 'i');
-    return 0;
-}
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
 
+	*buff_ind = 0;
+}
 
